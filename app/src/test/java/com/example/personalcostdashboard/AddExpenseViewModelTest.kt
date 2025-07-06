ba9e6f33b8312aa33040f8611530db8028e0b391
Shankar1371 <71003216+Shankar1371@users.noreply.github.com>
2025-07-06 11:13:55 -0500

    Add AddExpenseViewModelTest

---
+++ app/src/test/java/com/example/personalcostdashboard/AddExpenseViewModelTest.kt
@@ -0,0 +1,20 @@
+package com.example.personalcostdashboard
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.personalcostdashboard.ui.AddExpense.AddExpenseViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class AddExpenseViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun setDate_updatesSelectedDate() {
+        val viewModel = AddExpenseViewModel()
+        viewModel.setDate("1/1/2025")
+        assertEquals("1/1/2025", viewModel.selectedDate.value)
+    }
+}
